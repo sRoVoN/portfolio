b48d18d0ef3e167c6b623327537651931756653c
DESKTOP-IBAEHR8 <soodeh.arvin@gmail.com>
2025-09-07 23:08:12 +0330

    Fix font in contact page

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -4,7 +4,7 @@ import Contact from "@/app/components/contact/Contact";
 
 export default async function contactPage() {
   return (
-    <section className=" text-white py-12 px-6">
+    <section className=" text-black dark:text-white py-12 px-6">
       <Contact />
     </section>
   );
